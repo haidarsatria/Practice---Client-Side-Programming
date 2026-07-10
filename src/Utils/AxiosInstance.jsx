cdf26f5f010cc6bbe33028fef8927cd722ada250
haidarsatria <haidarsatria72@gmail.com>
2026-07-10 16:39:04 +0900

    fix: fix case sensitivity filename for vercel build

--- src/Utils/AxiosInstance.jsx
+++ src/Utils/AxiosInstance.jsx
@@ -1,7 +1,6 @@
 import axios from "axios";
 
 const AxiosInstance = axios.create({
-    // 🟢 CUKUP AMBIL VARIABEL `.env` NYA SAJA BRAY, KARENA DI .ENV SUDAH LENGKAP ADA /rest/v1
     baseURL: import.meta.env.VITE_SUPABASE_URL, 
     headers: {
         "Content-Type": "application/json",
